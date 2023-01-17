52db7b2746ffdaf581756052fed816127192bbe7
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-01-17 10:24:38 -0400

    refactor(login.service): delete async in the function login

--- src/services/login.service.js
+++ src/services/login.service.js
@@ -1,6 +1,6 @@
 import { http, httpFile } from "./http.service";
 
-export async function login(datos) {
+export function login(datos) {
   // datos (email, password)
   return http().post("/login", datos);
 }
