9b74f8cf0f7f7505b71a47ac3f296a81450194c6
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-01-17 12:10:17 -0400

    feat: install primeflex

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
-import "primevue/resources/primevue.min.css";
-import "";
-
 import { createApp } from "vue";
-import { createPinia } from "pinia";
+import PrimeVue from "primevue/config";
 
 import App from "./App.vue";
 import router from "./router";
@@ -11,7 +8,7 @@ import "./assets/main.css";
 
 const app = createApp(App);
 
-app.use(createPinia());
+app.use(PrimeVue);
 app.use(router);
 
 app.mount("#app");
