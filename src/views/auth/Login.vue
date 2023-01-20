afa81804dd4c94f70c3fe95179b0e23ac0f628ae
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-01-20 08:00:46 -0400

    refactor: edit the labels in the menu

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -56,13 +56,13 @@ export default {
                         <div class="text-900 text-3xl font-medium mb-3">Welcome, Isabel!</div>
                         <span class="text-600 font-medium">Sign in to continue</span>
                     </div>
-
+                    
                     <div>
                         <label for="email1" class="block text-900 text-xl font-medium mb-2">Email</label>
                         <InputText id="email1" type="text" placeholder="Email address" class="w-full md:w-30rem mb-5" style="padding: 1rem" v-model="usuario.email" />
 
                         <label for="password1" class="block text-900 font-medium text-xl mb-2">Password</label>
-                        <Password id="password1" v-model="usuario.password" placeholder="Password" :toggleMask="true" class="w-full mb-3" inputClass="w-full" inputStyle="padding:1rem"></Password>
+                        <Password id="password1" v-model="usuario.password" placeholder="Password" :toggleMask="true" class="w-full mb-3" inputClass="w-full" inputStyle="padding:1rem" ></Password>
 
                         <div class="flex align-items-center justify-content-between mb-5 gap-5">
                             <div class="flex align-items-center">
