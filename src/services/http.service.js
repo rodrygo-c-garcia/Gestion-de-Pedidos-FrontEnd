bd0861d56c54ca1341811395015f78cd0048ba1a
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-01-20 08:59:55 -0400

    feat: add function listarCategorias

--- src/services/http.service.js
+++ src/services/http.service.js
@@ -25,5 +25,4 @@ export function httpFile() {
   });
 }
 
-
 // Clase 18
--- src/views/admin/categoria/Categorias.vue
+++ src/views/admin/categoria/Categorias.vue
@@ -1,11 +1,28 @@
 <template>
   <h1>Estas en la lista de Categorias</h1>
+  <h2>Lista de Categorias</h2>
+  <h3> {{ categorias }}</h3>
 </template>
 
 <script>
+import * as apiCategoria from '@/services/categoria.service';
 
 export default {
+  data() {
+    return {
+      categorias: []
+    }
+  },
 
+  mounted() {
+    this.listarCategoria()
+  },
+  methods() {
+    async function listarCategoria() {
+      const { data } = await apiCategoria.getCategorias()
+      this.categorias = data
+    }
+  }
 }
 </script>
 
