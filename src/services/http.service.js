7f278341b8ae86db522798586fa43fdbe84c5920
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-01-27 22:00:36 -0400

    refactor: add encrptyon window.atob()

--- src/services/http.service.js
+++ src/services/http.service.js
@@ -1,14 +1,14 @@
+import Buffer from "buffer";
+
 // Para configurar nuestras peticiones globalmente
 import axios from "axios";
 import { url_base } from "@/config/index.js";
-import router from "@/router";
 
 // formamos el axios para que se conecte a la API
 export function http() {
   let token = "";
   try {
-    let tonken64 = localStorage.getItem("token");
-    let token = Buffer.from(tonken64, "base64").toString("ascii");
+    token = window.atob(localStorage.getItem("token"));
   } catch (e) {}
 
   // configuracion global de nuestra endpoint
@@ -17,7 +17,7 @@ export function http() {
     headers: {
       "Content-Type": "application/json",
       Accept: "application/json",
-      Authorization: "Bearer" + token,
+      Authorization: "Bearer " + token,
     },
   });
 
@@ -30,8 +30,8 @@ export function http() {
       //limipiamos el localStorage
       localStorage.clear();
       // redireccionamos al login cuando hay un error
-      //router.push("/login");
       return Promise.reject(error);
+      //router.push("/login");
     }
   );
   return interceptor;
@@ -41,7 +41,7 @@ export function http() {
 export function httpFile() {
   // configuracion global de nuestra endpoint
   return axios.create({
-    baseURL: "http://127.0.0.1:8000/api",
+    baseURL: url_base,
     headers: {
       "Content-Type": "multipart/form-data",
       Accept: "application/json",
