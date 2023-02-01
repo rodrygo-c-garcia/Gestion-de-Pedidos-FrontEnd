9b002e2cf71e458a4840c45770e5d774c4319fc9
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-01-31 20:51:03 -0400

    feat: add example the composition API

--- src/views/admin/pedido/Pedido.vue
+++ src/views/admin/pedido/Pedido.vue
@@ -1,14 +1,22 @@
 <template>
   <div class="card">
-    <h1>Pedidos</h1>
-    <h2> {{ $store.state.mensaje }}</h2>
+    <h1>Nuevo Pedido</h1>
+
+    <Button label="+" class="success" @click="incrementCounter" />
+    <h2>{{ counter }}</h2>
+    <Button label="-" class="danger" @click="decrementCounter" />
   </div>
 </template>
 
-<script>
-export default {
+<!-- Composition API -->
+<script setup>
+import { ref } from 'vue'
 
-}
+const counter = ref(0)
+
+const incrementCounter = () => { counter.value++ }
+const decrementCounter = () => { counter.value-- }
+
 </script>
 
 <style>
