f8521b28bec689e0708ead96142c302691ae3a70
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-02-03 15:39:38 -0400

    feat: add example basic of Pinia

--- src/store/index.js
+++ src/store/index.js
@@ -1,27 +1,57 @@
-import { createStore } from "vuex";
+// stores/counter.js
+import { defineStore } from "pinia";
 
-export default createStore({
-  // los estados son los datos globales
-  state: {
-    mensaje: "Hello from Vuex",
+export const usePiniaStore = defineStore("store", {
+  state: () => {
+    return {
+      count: 0,
+      titulo: "Cambio de estado con PINIA",
+    };
   },
-  mutations: {
-    cambioStateMsj(state, valor) {
-      state.mensaje = valor;
-    },
-  },
+  // could also be defined as
+  // state: () => ({ count: 0 })
   actions: {
-    // nuestro metodo accion recibe un contexto, que a su vez este va a llamar a cambioStateMsj
-    cambiarMsj(context, valor) {
-      context.commit("cambioStateMsj", valor);
+    incrementar() {
+      this.count++;
+    },
+    decrementar() {
+      this.count--;
+    },
+    changeMSJ(msg) {
+      this.titulo = msg;
     },
   },
-  modules: {},
-  // ayudan a capturar los estados
   getters: {
-    // funcion
-    getMensaje(state) {
-      return state.mensaje;
+    parImpar: (state) => {
+      return state.count % 2 === 0 ? "par" : "impar";
     },
   },
 });
+
+// import { createStore } from "vuex";
+
+// export default createStore({
+//   // los estados son los datos globales
+//   state: {
+//     mensaje: "Hello from Vuex",
+//   },
+//   mutations: {
+//     cambioStateMsj(state, valor) {
+//       state.mensaje = valor;
+//     },
+//   },
+//   actions: {
+//     // nuestro metodo accion recibe un contexto, que a su vez este va a llamar a cambioStateMsj
+//     cambiarMsj(context, valor) {
+//       context.commit("cambioStateMsj", valor);
+//     },
+//   },
+//   modules: {},
+//   // ayudan a capturar los estados
+//   getters: {
+//     // funcion
+//     getMensaje(state) {
+//       return state.mensaje;
+//     },
+//   },
+// });
