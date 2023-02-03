900e44cab980ca88fc5de5478e9e937c00dc9cba
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-02-03 19:06:52 -0400

    feat: add welcome user admin

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,13 @@
 <template>
   <main>
     <h2>Mi componente Home (ADMINISTRADOR)</h2>
+    <div class="card">
+      <h5>Bienvenido {{ pinia.user.name }}</h5>
+    </div>
   </main>
 </template>
+
+<script setup>
+import { usePiniaStore } from '@/store/index.js'
+const pinia = usePiniaStore()
+</script>
