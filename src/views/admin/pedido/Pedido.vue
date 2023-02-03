a759dc0d19ea44f4affeac42ceeacf6f14940c18
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-02-03 15:40:07 -0400

    feat: add example of Pinia (state, getters)

--- src/views/admin/pedido/Pedido.vue
+++ src/views/admin/pedido/Pedido.vue
@@ -1,21 +1,26 @@
 <template>
   <div class="card">
     <h1>Pedido</h1>
+    <h2>{{ pinia.titulo }}</h2>
 
-    <Button label="+" class="success" @click="incrementCounter" />
-    <h2>{{ counter }}</h2>
-    <Button label="-" class="danger" @click="decrementCounter" />
+    <h3>{{ pinia.count }} es {{ pinia.parImpar }}</h3>
+
+    <Button @click="pinia.incrementar">+</Button>
+    <Button @click="pinia.decrementar">-</Button>
+
+    <div>
+      <input v-model="msg" type="text">
+      <Button label="cambiar" @click="pinia.changeMSJ(msg)" type="text" />
+    </div>
   </div>
 </template>
 
 <!-- Composition API -->
 <script setup>
-import { ref } from 'vue'
+import { usePiniaStore } from '@/store/index.js'
 
-const counter = ref(0)
-
-const incrementCounter = () => { counter.value++ }
-const decrementCounter = () => { counter.value-- }
+const pinia = usePiniaStore()
+const msg = ''
 
 </script>
 
